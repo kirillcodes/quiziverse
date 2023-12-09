4c0832ce3e02d7462358218eb3850c2a8e2e1287
Kirill <kirillusernet@gmail.com>
2023-12-09 10:50:43 +0100

    styled auth form

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { CustomInput } from "@components";
 import Logo from "@assets/images/logo-universe.png";
 
@@ -8,23 +8,40 @@ import { CustomButton } from "@components/CustomButton";
 export const Auth: React.FC = () => {
   const emailDomain: string = import.meta.env.VITE_STUDENT_EMAIL_DOMAIN;
 
+  const [authMethod, setAuthMethod] = useState("login");
+
+  const handleChoiceMethod = (method: string) => {
+    setAuthMethod(method);
+  };
+
   return (
     <div className={scss.auth}>
       <div className={scss.container}>
         <img src={Logo} alt="Quiziverse" />
-        <h2>Authorization</h2>
+        <h1>Authorization</h1>
         <div className={scss.choiceMethod}>
-          <div>Login</div>
-          <span></span>
-          <div>Registration</div>
+          <div
+            onClick={() => handleChoiceMethod("login")}
+            style={authMethod === "login" ? { backgroundColor: "var(--light-color)" } : {}}
+          >
+            Login
+          </div>
+          <div
+            onClick={() => handleChoiceMethod("registration")}
+            style={authMethod === "registration" ? { backgroundColor: "var(--light-color)" } : {}}
+          >
+            Registration
+          </div>
         </div>
         <form>
+          <h3>Email:</h3>
           <CustomInput
             type="email"
             placeholder={`kirillcodes@${emailDomain ? emailDomain : "quiziverse.com"}`}
           />
-          <CustomInput type="password" placeholder="Password" />
-          <CustomButton title="Registration" />
+          <h3>Password:</h3>
+          <CustomInput type="password" placeholder="••••••••••" />
+          <CustomButton title={authMethod === "login" ? "Sign-In" : "Sign-Up"} />
         </form>
       </div>
     </div>
