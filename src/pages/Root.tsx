b509a96a7d4e4d24489f4630eeeec934226fb9c9
Kirill <kirillusernet@gmail.com>
2023-12-11 21:24:14 +0100

    added redirect to auth

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,15 +1,26 @@
-import { Outlet } from "react-router-dom";
+import { NavigateFunction, Outlet, useNavigate } from "react-router-dom";
 import { Navbar } from "@layouts";
 import scss from "@styles/pages/Root.module.scss";
 import { Container } from "@components";
+import { useAppSelector } from "@hooks/useAppSelector";
+import { useEffect } from "react";
 
 export const Root = () => {
-  return (
+  const token: boolean = useAppSelector((state) => !!state.auth.loginData.accessToken);
+  const navigate: NavigateFunction = useNavigate();
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/auth");
+    }
+  }, [token, navigate]);
+
+  return token ? (
     <main className={scss.main}>
       <Navbar />
       <Container>
         <Outlet />
       </Container>
     </main>
-  );
+  ) : null;
 };
