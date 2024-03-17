2e1c765bbac1cef2fe012d0e60fc80e20c911d35
Kirill <kirillusernet@gmail.com>
2024-03-17 17:15:12 +0100

    feat(testsApi): provides and invalidates tags

--- src/store/api/testsApi.ts
+++ src/store/api/testsApi.ts
@@ -4,6 +4,7 @@ import { CreateTestDto } from "@dto/create-test.dto";
 
 export const testsApi = createApi({
   reducerPath: "testsApi",
+  tagTypes: ["TestsList"],
   baseQuery: interceptorQuery,
   endpoints: (builder) => ({
     createTest: builder.mutation<CreateTestDto, { courseId: string }>({
@@ -12,9 +13,17 @@ export const testsApi = createApi({
         method: "POST",
         body,
       }),
+      invalidatesTags: [{ type: "TestsList", id: "LIST" }],
     }),
     getTests: builder.query({
       query: (courseId: string) => `courses/${courseId}/tests`,
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map(({ id }: { id: number }) => ({ type: "TestsList" as const, id })),
+              { type: "TestsList", id: "LIST" },
+            ]
+          : [{ type: "TestsList", id: "LIST" }],
     }),
   }),
 });
