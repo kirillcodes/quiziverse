b76ffb8d89e4cecd2d9a7c8bfb7b043ea53ccf43
Kirill <kirillusernet@gmail.com>
2024-03-18 14:43:52 +0100

    feat(TestPage): select answer

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,13 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import scss from "@styles/pages/TestPage.module.scss";
 import { useGetTestQuery } from "@store/api/testsApi";
 import { useNavigate, useParams } from "react-router-dom";
 
+type PostData = {
+  [key: number]: number;
+};
+
 export const TestPage: React.FC = () => {
   const navigate = useNavigate();
   const { courseId, testId } = useParams();
-
   const { data, isLoading, isError } = useGetTestQuery({ courseId, testId });
+  const [postData, setPostData] = useState<PostData>({});
 
   useEffect(() => {
     if (isError || data?.status === 404) {
@@ -15,22 +19,38 @@ export const TestPage: React.FC = () => {
     }
   }, [isError, navigate, data]);
 
+  const selectAnswer = (quesitonId: number, answerIndex: number) => {
+    setPostData((prevData) => ({
+      ...prevData,
+      [quesitonId]: answerIndex,
+    }));
+  };
+
   if (isLoading) return <div>Loading...</div>;
+
   return (
     <div className={scss.testPage}>
       <h2>{data?.title}</h2>
-
       <div className={scss.questionsList}>
-        {data?.questions.map((question) => (
+        {data?.questions.map((question, questionIndex) => (
           <div className={scss.question} key={question.id}>
-            <h3>{question.text}</h3>
-            <div className={scss.answersList}>
-              {question.answers.map((answer, index) => (
-                <div className={scss.answer} key={index}>
+            <h3>
+              {questionIndex + 1}. {question.text}
+            </h3>
+            <ul className={scss.answersList}>
+              {question.answers.map((answer, answerIndex) => (
+                <li className={scss.answer} key={answerIndex}>
+                  <input
+                    type="radio"
+                    className={scss.answerRadio}
+                    value={answerIndex}
+                    checked={postData[question.id] === answerIndex}
+                    onChange={() => selectAnswer(question.id, answerIndex)}
+                  />
                   {answer.text}
-                </div>
+                </li>
               ))}
-            </div>
+            </ul>
           </div>
         ))}
       </div>
