19ebc83254b93c8f4d1cf6bb6babf9630b7ce6f2
Kirill <kirillusernet@gmail.com>
2024-03-25 13:35:35 +0100

    feat(auth): get role

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -3,17 +3,24 @@ import { Navbar } from "@layouts";
 import scss from "@styles/pages/Root.module.scss";
 import { Container } from "@components";
 import { useEffect } from "react";
+import { useGetRoleQuery } from "@store/api/authApi";
 
 export const Root = () => {
   const navigate = useNavigate();
 
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
 
   useEffect(() => {
     if (!token) {
       navigate("/auth");
     }
   }, [navigate, token]);
+
+  const { refetch: refetchRole } = useGetRoleQuery({});
+
+  useEffect(() => {
+    refetchRole();
+  }, [refetchRole]);
 
   return (
     token && (
